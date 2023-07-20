e5676b053a89eece19dc3db6fad44a4f8822fdf3
wangzhuowei <wangzhuowei@bytedance.com>
2023-07-20 18:01:34 +0800

    feat: add threads benchmark

--- util/tpool/thread_pool_test.go
+++ util/tpool/thread_pool_test.go
@@ -84,3 +84,77 @@ func BenchmarkIOTasks(b *testing.B) {
 		})
 	}
 }
+
+func BenchmarkGoroutineVsThreads(b *testing.B) {
+	runtime.GOMAXPROCS(2)
+	ioWork := func() {
+		time.Sleep(time.Millisecond * 10)
+	}
+	cpuWork := func() {
+		var mem [1000]int
+		for i := 0; i < 100000; i++ {
+			mem[i%len(mem)] += i
+		}
+		_ = mem
+	}
+	type benchcase struct {
+		kind   string
+		runner func(workload func(), count int)
+		count  int
+	}
+	var benchcases = []benchcase{
+		{kind: "Goroutines", runner: goroutineRunner, count: 1},
+		{kind: "Goroutines", runner: goroutineRunner, count: 4},
+		{kind: "Goroutines", runner: goroutineRunner, count: 8},
+		{kind: "Goroutines", runner: goroutineRunner, count: 16},
+		{kind: "Goroutines", runner: goroutineRunner, count: 32},
+		{kind: "Goroutines", runner: goroutineRunner, count: 100},
+
+		{kind: "Threads", runner: threadRunner, count: 1},
+		{kind: "Threads", runner: threadRunner, count: 4},
+		{kind: "Threads", runner: threadRunner, count: 8},
+		{kind: "Threads", runner: threadRunner, count: 16},
+		{kind: "Threads", runner: threadRunner, count: 32},
+		{kind: "Threads", runner: threadRunner, count: 100},
+	}
+	for _, c := range benchcases {
+		b.Run(fmt.Sprintf("%s[%d]", c.kind, c.count), func(b *testing.B) {
+			b.Run("IO Work", func(b *testing.B) {
+				for i := 0; i < b.N; i++ {
+					c.runner(ioWork, c.count)
+				}
+			})
+			b.Run("CPU Work", func(b *testing.B) {
+				for i := 0; i < b.N; i++ {
+					c.runner(cpuWork, c.count)
+				}
+			})
+		})
+	}
+}
+
+func goroutineRunner(workload func(), count int) {
+	var wg sync.WaitGroup
+	for t := 0; t < count; t++ {
+		wg.Add(1)
+		go func() {
+			workload()
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+}
+
+var testThreadPool = NewCachedThreadPool(WithCachedMaxIdleThreads(32))
+
+func threadRunner(workload func(), count int) {
+	var wg sync.WaitGroup
+	for t := 0; t < count; t++ {
+		wg.Add(1)
+		testThreadPool.Submit(func() {
+			workload()
+			wg.Done()
+		})
+	}
+	wg.Wait()
+}
